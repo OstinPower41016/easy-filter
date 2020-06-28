0bb2e289e05c16aabafb7a10e340ff3b1cf6f8e1
Nikolay Larionov <63050019+OstinPower41016@users.noreply.github.com>
2020-06-28 17:19:31 +0300

    Update filter.js

--- filter.js
+++ filter.js
@@ -1,12 +1,10 @@
-const filter = (menuSelector, wrapperSelector, noItemProfile) => {
+const filter = (menuSelector, wrapperSelector, noItemPortfolio) => {
    const menu = document.querySelector(menuSelector),
          wrapper = document.querySelector(wrapperSelector),
-         no = document.querySelector(noItemProfile);
-
+         no = document.querySelector(noItemPortfolio);
 
    menu.children.forEach(btn => btn.addEventListener('click', (e) =>  {
       menu.children.forEach(rem => rem.classList.remove('active'));
-      // btn.classList.remove('active');
       
       no.classList.add('animated');
       no.classList.remove('fadeIn');
@@ -14,27 +12,24 @@ const filter = (menuSelector, wrapperSelector, noItemProfile) => {
 
       const target = e.target,
             targetClassName = target.classList.value;
-      
 
+      target.classList.add('active');
+      
       wrapper.children.forEach(item => {
          item.classList.add('animated');
          item.classList.remove('fadeIn');
          item.style.display = 'none';
 
          if (item.classList.contains(targetClassName)) {
-            target.classList.add('active');
             item.style.display = 'block';
             item.classList.add('fadeIn');
          } else {
-            target.classList.add('active');
             no.classList.add('fadeIn');
             no.style.display = 'block';
          }
 
       });
-
    }));
-
 };
 
 export default filter;
